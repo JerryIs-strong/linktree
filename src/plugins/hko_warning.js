d31c86fd36c2e9159319e9c0003cd727800bdf85
Jerry7818 <96361414+JerryIs-strong@users.noreply.github.com>
2025-06-29 10:26:41 +0800

    Update hko_warning.js

--- src/plugins/hko_warning.js
+++ src/plugins/hko_warning.js
@@ -11,7 +11,7 @@ async function getData() {
                     warningElement.className = 'warning_sign';
                     warningElement.alt = warning.code;
                     warningElement.title = `${warning.name}現正生效 - 資訊來自data.gov.hk`;
-                    warningElement.src = `/src/data/warning_sign/${warning.code}.gif`;
+                    warningElement.src = `../data/warning_sign/${warning.code}.gif`;
                     container.appendChild(warningElement);
                 }
             });
