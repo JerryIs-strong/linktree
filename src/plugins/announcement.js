f130e5b13cd10e511dc1bbe754fc0e32e9e31935
JerryIs-strong <heyajerry@gmail.com>
2025-07-05 20:37:11 +0800

    Enhance styling and add new assets
    - Added a title gradient to both light and dark themes in style.css.
    - Adjusted margins and z-index for the page title for better layout.
    - Introduced a new CSS rule for the first letter of the page title to apply the gradient.
    - Updated image styles for better responsiveness.
    - Added webkit-prefixed backdrop-filter for compatibility across browsers.
    - Created a new .hintrc file to configure linting rules for CSS.
    - Added a new arrow image asset in WEBP format for improved performance.

--- src/plugins/announcement.js
+++ src/plugins/announcement.js
@@ -1,27 +1,28 @@
 function closeAnnouncement() {
-  const announcement_wrapper = document.querySelector('.announcement_wrapper');
-  const announcement_mask = document.querySelector('.announcement_mask');
+  const announcement_wrapper = document.querySelector(".announcement_wrapper");
+  const announcement_mask = document.querySelector(".announcement_mask");
   announcement_wrapper.remove();
   announcement_mask.remove();
 }
 
 function showAnnouncement(AnnouncementData, noExpireDate = false) {
-  const announcement_wrapper = document.getElementById('announcement_wrapper');
-  const announcement_title = document.createElement('div');
-  const announcement_content = document.createElement('div');
-  const announcement_close = document.createElement('div');
-  const announcement_mask = document.getElementById('announcement_mask');
+  const announcement_wrapper = document.getElementById("announcement_wrapper");
+  const announcement_title = document.createElement("div");
+  const announcement_content = document.createElement("div");
+  const announcement_close = document.createElement("div");
+  const announcement_mask = document.getElementById("announcement_mask");
 
-  announcement_title.className = 'announcement-title';
-  announcement_content.className = 'announcement-content';
-  announcement_close.className = 'announcement-close';
+  announcement_title.className = "announcement-title";
+  announcement_content.className = "announcement-content";
+  announcement_close.className = "announcement-close";
 
   announcement_title.innerText = AnnouncementData.title;
   if (!noExpireDate) {
     announcement_title.innerHTML += `<div class="announcement-hashtag">#有期限: ${AnnouncementData.expireDate}</div>`;
   }
   announcement_content.innerHTML = AnnouncementData.content;
-  announcement_close.innerHTML = '<span class="material-symbols-outlined"> close </span>';
+  announcement_close.innerHTML =
+    '<span class="material-symbols-outlined"> close </span>';
   announcement_close.onclick = () => {
     closeAnnouncement();
   };
@@ -32,7 +33,7 @@ function showAnnouncement(AnnouncementData, noExpireDate = false) {
   announcement_wrapper.appendChild(announcement_title);
 
   if (AnnouncementData.picture) {
-    const announcement_picture = document.createElement('img');
+    const announcement_picture = document.createElement("img");
     announcement_picture.src = AnnouncementData.picture;
     announcement_wrapper.appendChild(announcement_picture);
   }
@@ -41,9 +42,9 @@ function showAnnouncement(AnnouncementData, noExpireDate = false) {
   announcement_wrapper.appendChild(announcement_close);
 
   if (AnnouncementData.button) {
-    const announcement_button = document.createElement('a');
+    const announcement_button = document.createElement("a");
     const buttonData = AnnouncementData.button;
-    announcement_button.className = 'announcement-button';
+    announcement_button.className = "announcement-button";
     announcement_button.innerText = AnnouncementData.button.text;
     announcement_button.style.backgroundColor = AnnouncementData.button.color;
 
@@ -55,23 +56,23 @@ function showAnnouncement(AnnouncementData, noExpireDate = false) {
       } else if (buttonData.onclickAction.includes("link: ")) {
         const link = buttonData.onclickAction.split("link: ")[1];
         closeAnnouncement();
-        window.open(link, '_blank');
+        window.open(link, "_blank");
       }
-    }
+    };
     announcement_wrapper.appendChild(announcement_button);
   }
 }
 
-fetch('./src/data/announcement.json')
+fetch("./src/data/announcement.json")
   .then((response) => {
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     return response.json();
   })
   .then((data) => {
     const today = {
-      date: new Date().toISOString().split('T')[0]
+      date: new Date().toISOString().split("T")[0],
     };
 
     if (data.announce.expireDate === "/") {
@@ -82,5 +83,4 @@ fetch('./src/data/announcement.json')
     } else {
       closeAnnouncement();
     }
-
-  })
+  });
